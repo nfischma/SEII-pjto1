95cff2ba65ca3cc38abee40f1ead8ab39c38d0e3
RamarangGH <nicolas-fischmann@hotmail.com>
2022-01-30 22:13:18 -0300

    background

--- main.py
+++ main.py
@@ -4,12 +4,21 @@ pygame.init()
 
 #criação janela
 pygame.display.set_caption("Simulação drone")
-pygame.display.set_mode((1620,960))
+screen = pygame.display.set_mode((1620,960))
+
+#criação background
+background = pygame.image.load('assets/background.png')
 
 running = True
 
 
 while running:
+    #mostrar o background
+    screen.blit(background, (0, 0))
+
+    #atualizar a tela
+    pygame.display.flip()
+
 
     for event in pygame.event.get():
         #fecha o jogo se o jogador fecha a janela
