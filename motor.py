42c946a54a8f38c84b265d28183aa07514fe374b
RamarangGH <nicolas-fischmann@hotmail.com>
2022-02-05 17:26:23 -0300

    atualização assets

--- motor.py
+++ motor.py
@@ -6,7 +6,7 @@ class Motor(pygame.sprite.Sprite):
 
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load('assets/drone/motor_1.png')
-        self.rect = self.image.get_rect()
+        #self.image = pygame.image.load('assets/drone/motor_1.png')
+        #self.rect = self.image.get_rect()
 
 
