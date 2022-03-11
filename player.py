6c59e4ababe97ea2c7267e029ecce857b062cda8
RamarangGH <nicolas-fischmann@hotmail.com>
2022-03-10 21:04:29 -0300

    end

--- player.py
+++ player.py
@@ -44,8 +44,8 @@ class Player(pygame.sprite.Sprite):
         #listas posiçoes
         self.r = np.array([[0.0,0.0],[0.0,0.0]])
         #comando de posicao
-        self.rbarra = [0.0,100.0]
-
+        self.rbarra = [10.0,10.0]
+        self.lrbarra = np.transpose(np.array([self.rbarra]))
         #v
         #velocidade linear
         self.velocity = [0.0, 0.0]
@@ -114,8 +114,8 @@ class Player(pygame.sprite.Sprite):
         self.pos_rot[1] = self.rect.y-self.rot.get_rect().height/2
 
         #atualizar posicao
-        self.pos[0] = self.pos_rot[0] + self.r[:,-1][0]/10
-        self.pos[1] = self.pos_rot[1] + self.r[:,-1][1]/10
+        self.pos[0] = self.pos_init[0] + self.r[:,-1][0]
+        self.pos[1] = self.pos_init[1] - self.r[:,-1][1]
         #atualizar rect
         self.rect.y = self.pos[1]+self.rot.get_rect().height/2
         self.rect.x = self.pos[0]+self.rot.get_rect().width/2 
@@ -257,75 +257,16 @@ class Player(pygame.sprite.Sprite):
         print("test :", [wp[0],wp[1], rp[0],rp[1], vp[0],vp[1], phip, dphip])
         return [wp[0],wp[1], rp[0],rp[1], vp[0],vp[1], phip, dphip]
 
-    def rk4(self, f, y0, t, args=()):
-        n = len(t)
-        y = np.zeros((n, len(y0)))
-        y[0] = y0
-        for i in range(n - 1):
-            h = t[i+1] - t[i]
-            k1 = np.array(f(y[i], t[i], *args))
-            k2 = np.array(f(y[i] + k1 * h / 2., t[i] + h / 2., *args))
-            k3 = np.array(f(y[i] + k2 * h / 2., t[i] + h / 2., *args))
-            k4 = np.array(f(y[i] + k3 * h, t[i] + h, *args))
-            y[i+1] = y[i] + (h / 6.) * (k1 + 2*k2 + 2*k3 + k4)
-        return y
-
-    def rk4_2(self, tk, h, xk, uk):
-        k1 = x_dot(tk , xk , uk)
-        k2 = x_dot(tk + h/2.0, xk + h*k1/2.0, uk)
-        k3 = x_dot(tk + h/2.0, xk + h*k2/2.0, uk)
-        k4 = x_dot(tk + h , xk + h*k3 , uk)
-        xkp1 = xk +(h/6.0)*(k1 + 2*k2 + 2*k3 + k4)
+    def rk4(self, tk, h, xk, uk):
+        k1 = np.array(self.x_dot(tk, xk, uk))
+        k2 = np.array(self.x_dot(tk+h/2.0, xk+h*k1/2.0, uk))
+        k3 = np.array(self.x_dot(tk+h/2.0, xk+h*k2/2.0, uk))
+        k4 = np.array(self.x_dot(tk+h, xk+h*k3, uk))
+        xkp1 = xk + (h / 6.) * (k1 + 2*k2 + 2*k3 + k4)
         return xkp1
 
-    def atualizar_dinamica(self, tempo):
-        #posicao e angulo na tela
-        self.atualizar_posicao_tela()
 
-        #FC e phibarra
-        self.Cp()
-        #Tc
-        self.Ca()
-        #wbarras
-        self.Cw()
-        
-
-        #dinamica robo
-        # Evoluindo a din. da planta
-        #condições iniciais
-        w0 = self.w
-        w0 = w0.tolist()
-        r0 = self.r[:,-1]
-        r0[1] = r0[1]
-        r0 = r0.tolist()
-        v0 = self.dr[:,-1]
-        v0[1] = v0[1]
-        v0 = v0.tolist()
-        phi0 = self.phi[-1]
-        dphi0 = self.dphi[-1]
-        x0 = [w0[0], w0[1], r0[0], r0[1], v0[0], v0[1], phi0, dphi0]   # condicao inicial
-
-        #TENTAR COM RK4
-        #calculo eq. dif.
-        sol = self.rk4(self.din_robo, x0, [self.t[-1], self.t[-1]+tempo], args = (self.wbarra, tempo))
-        #sol = odeint(self.din_robo, x0, [0.0, tempo], args=(self.wbarra,tempo,))
-        print("[w0[0], w0[1], r0[0], r0[1], v0[0], v0[1], phi0, dphi0] ")
-        print(x0)
-        print(sol[:,0][-1], sol[:,1][-1], sol[:,2][-1], sol[:,3][-1], sol[:,4][-1], sol[:,5][-1], sol[:,6][-1], sol[:,7][-1])
-        #solucao eq. dif.
-        self.w[0] = sol[:,0][-1]
-        self.w[1] = sol[:,1][-1]
-        r1 = np.transpose(np.array([[sol[:,2][-1],sol[:,3][-1]]]))
-        self.r = np.concatenate((self.r,r1), axis=1)
-        dr1 = np.transpose(np.array([[sol[:,4][-1],sol[:,5][-1]]]))
-        self.dr = np.concatenate((self.dr,dr1), axis=1)
-        self.phi += [sol[:,6][-1]]
-        self.dphi += [sol[:,7][-1]]
-        self.t += [self.t[-1] + tempo]
-
-        self.rebote()
-
-    def x_dot(t, x, w_):
+    def x_dot(self, t, x, w_):
         # State vector
         # x = [ w r_xy v_xy phi omega]' \in R^8
         #print('x: ', x)
@@ -374,153 +315,234 @@ class Player(pygame.sprite.Sprite):
         return xkp1
     
 
-    def atualizar_dinamica_2():
-        pass
-        # # PARÂMETROS DE SIMULAÇÃO
-        # h = 2.5e-3 # passo da simulação de tempo continuo
-        # Ts = 10e-3 # intervalo de atuação do controlador
-        # fTh = Ts/h
-        # maxT = 60
-        # tc = np.arange(0,maxT,h) # k
-        # td = np.arange(0,maxT,Ts) # j
-        # tam = len(tc)
-        # j = 0
-        # # Vetor de estados
-        # # State vector
-        # # x = [ w r_xy v_xy phi omega]' \in R^8
-        # x = np.zeros([8, tam])
-        # #x[:,0] = np.array([0.,1.,2,3,4,5,6,7,])
-        # x[:,0] = np.array([0., 0., \
-        #     0., 0., \
-        #     0., .0, \
-        #     0*np.pi/180., \
-        #     0*np.pi/180.])
+    def atualizar_dinamica(self, tempo):
+        self.atualizar_posicao_tela()
+        # PARÂMETROS DE SIMULAÇÃO
+        h = 2.5e-3 # passo da simulação de tempo continuo
+        Ts = 10e-3 # intervalo de atuação do controlador
+        fTh = Ts/h
+        maxT = tempo
+        tc = np.arange(0,maxT,h) # k
+        td = np.arange(0,maxT,Ts) # j
+        tam = len(tc)
+        j = 0
+        # Vetor de estados
+        # State vector
+        # x = [ w r_xy v_xy phi omega]' \in R^8
+        x = np.zeros([8,tam])
+        #x[:,0] = np.array([0.,1.,2,3,4,5,6,7,])
+        w0 = self.w
+        w0 = w0.tolist()
+        r0 = self.r[:,-1]
+        r0[1] = r0[1]
+        r0 = r0.tolist()
+        v0 = self.dr[:,-1]
+        v0[1] = v0[1]
+        v0 = v0.tolist()
+        phi0 = self.phi[-1]*math.pi/180
+        dphi0 = self.dphi[-1]*math.pi/180
+        x0 = [w0[0], w0[1], r0[0], r0[1], v0[0], v0[1], phi0, dphi0]   # condicao inicial
 
-        # # Parâmetros do sistema de controle
-        # # Vetor de controle relativo à rotação
-        # w_ = np.zeros([2,len(td)]) # comando de controle
-        # # Vetor dos erros de posição
-        # eP_ = np.zeros([2,len(td)])
-        # ePm1 = 0 # erro posição k-1 (passo anterior)
-        # eVm1 = 0 # erro atitude k-1 (passo anterior)
-        # # Constanstes do modelo
-        # m = 0.25 # massa
-        # g = 9.81 # aceleração da gravidade
-        # l = 0.1 # tamanho
-        # kf = 1.744e-08 # constante de força
-        # Iz = 2e-4 # momento de inércia
-        # tal = 0.05
-        # Fe = np.array([-m*g])
-        # # Restrições do controle
-        # phi_max = 15*np.pi/180. # ângulo máximo
-        # w_max = 15000
-        # Fc_max = kf*w_max**2 # Força de controle máximo
-        # Tc_max = l*kf*w_max**2
-        # #Fc_min = 0.1 * Fc_max
-        # #Fc_max = 0.9 * Fc_max
-        # # Waypoints
-        # r_ = np.array([ [0.,10.], \
-        #     [15.,10.], \
-        #     [-50.,2.], \
-        #     [-20., 15.], \
-        #     [ 10., 0.]]).transpose()
-        # r_ID = 0
-        # r_IDN = 4
+        x[:,0] = np.array(x0)
+        # Parâmetros do sistema de controle
+        # Vetor de controle relativo à rotação
+        w_ = np.zeros([2,len(td)]) # comando de controle
+        # Vetor dos erros de posição
+        eP_ = np.zeros([2,len(td)])
+        ePm1 = 0 # erro posição k-1 (passo anterior)
+        eVm1 = 0 # erro atitude k-1 (passo anterior)
+        # Constanstes do modelo
+        m = 0.25 # massa
+        g = 9.81 # aceleração da gravidade
+        l = 0.1 # tamanho
+        kf = 1.744e-08 # constante de força
+        Iz = 2e-4 # momento de inércia
+        tal = 0.05
+        Fe = np.array([-m*g])
+        # Restrições do controle
+        phi_max = 15*np.pi/180. # ângulo máximo
+        w_max = 15000
+        Fc_max = kf*w_max**2 # Força de controle máximo
+        Tc_max = l*kf*w_max**2
+        #Fc_min = 0.1 * Fc_max
+        #Fc_max = 0.9 * Fc_max
+        # Waypoints
+        r_ = np.array([self.rbarra])
+        r_ID = 0
+        r_IDN = 0
 
-        # ### Execução da simulação
-        # for k in range(tam-1):
-        #     # Sistema de controle
-        #     if (k % fTh) == 0:
-        #         # Extrai os dados do vetor
-        #         r_k = x[2:4,k]
-        #         v_k = x[4:6,k]
-        #         phi_k = x[6,k]
-        #         ome_k = x[7,k]
-        #         # Comando de posição
-        #         v_ = np.array([0,0])
-        #         #####################
-        #         # Controle de Posição
-        #         kpP = np.array([.075])
-        #         kdP = np.array([0.25])
-        #         eP = r_[:,r_ID] - r_k
-        #         eV = v_ - v_k
-        #         eP_[:,j] = eP
-        #         #print(eP, eV)
-        #         # Definição do próximo waypoint
-        #         if np.linalg.norm(eP) < .1 and r_ID < (r_IDN):
-        #             r_ID += 1
-        #             print("Bucar Waypoint: ", r_ID)
-        #         Fx = kpP * eP[0] + kdP * eV[0]
-        #         Fy = kpP * eP[1] + kdP * eV[1] - Fe
-        #         Fy = np.maximum(0.2*Fc_max, np.minimum(Fy, 0.8*Fc_max))
-        #         #####################
-        #         # Controle de Atitude
-        #         phi_ = np.arctan2(-Fx, Fy)
-        #         if np.abs(phi_) > phi_max:
-        #             #print(phi_*180/np.pi)
-        #             signal = phi_/np.absolute(phi_)
-        #             phi_ = signal * phi_max
-        #             # Limitando o ângulo
-        #             Fx = Fy * np.tan(phi_)
-        #         Fxy = np.array([Fx, Fy])
-        #         Fc = np.linalg.norm(Fxy)
-        #         f12 = np.array([Fc/2.0, Fc/2.0])
-        #         # Constantes Kp e Kd
-        #         kpA = np.array([.75])
-        #         kdA = np.array([0.05])
-        #         ePhi = phi_ - phi_k
-        #         eOme = 0 - ome_k
-        #         Tc = kpA * ePhi + kdA * eOme
-        #         Tc = np.maximum(-0.4*Tc_max, np.minimum(Tc, 0.4*Tc_max))
-        #         # Delta de forças
-        #         df12 = np.absolute(Tc)/2.0
-        #         # Forças f1 e f2 final f12' = f12 + deltf12
-        #         if (Tc >= 0.0):
-        #             f12[0] = f12[0] + df12
-        #             f12[1] = f12[1] - df12
-        #         else:
-        #             f12[0] = f12[0] - df12
-        #             f12[1] = f12[1] + df12
-        #         # Comando de rpm dos motores
-        #         w1_ = np.sqrt(f12[0]/(kf))
-        #         w2_ = np.sqrt(f12[1]/(kf))
-        #         #
-        #         # Limitando o comando do motor entre 0 - 15000 rpm
-        #         w1 = np.maximum(0., np.minimum(w1_, w_max))
-        #         w2 = np.maximum(0., np.minimum(w2_, w_max))
-        #         # Determinação do comando de entrada
-        #         w_[:,j] = np.array([w1, w2])
-        #         j = j+1
-        #     # Simulação um passo a frente
-        #     x[:,k+1] = self.rk4(tc[k], h, x[:,k], w_[:,j-1])
+        ### Execução da simulação
+        for k in range(tam-1):
+            # Sistema de controle
+            if (k % fTh) == 0:
+                # Extrai os dados do vetor
+                r_k = x[2:4,k]
+                v_k = x[4:6,k]
+                phi_k = x[6,k]
+                ome_k = x[7,k]
+                # Comando de posição
+                v_ = np.array([0,0])
+                #####################
+                # Controle de Posição
+                kpP = np.array([0.075])
+                kdP = np.array([0.25])
+                eP = r_[:,r_ID] - r_k
+                eV = v_ - v_k
+                eP_[:,j] = eP
+                #print(eP, eV)
+                # Definição do próximo waypoint
+                if np.linalg.norm(eP) < .1 and r_ID < (r_IDN):
+                     r_ID += 1
+                #     print("Bucar Waypoint: ", r_ID)
+                Fx = kpP * eP[0] + kdP * eV[0]
+                Fy = kpP * eP[1] + kdP * eV[1] - Fe
+                Fy = np.maximum(0.2*Fc_max, np.minimum(Fy, 0.8*Fc_max))
+                #####################
+                # Controle de Atitude
+                phi_ = np.arctan2(-Fx, Fy)
+                if np.abs(phi_) > phi_max:
+                    #print(phi_*180/np.pi)
+                    signal = phi_/np.absolute(phi_)
+                    phi_ = signal * phi_max
+                    # Limitando o ângulo
+                    Fx = Fy * np.tan(phi_)
+                Fxy = np.array([Fx, Fy])
+                Fc = np.linalg.norm(Fxy)
+                f12 = np.array([Fc/2.0, Fc/2.0])
+                # Constantes Kp e Kd
+                kpA = np.array([.75])
+                kdA = np.array([0.02])
+                ePhi = phi_ - phi_k
+                eOme = 0 - ome_k
+                Tc = kpA * ePhi + kdA * eOme
+                Tc = np.maximum(-0.4*Tc_max, np.minimum(Tc, 0.4*Tc_max))
+                # Delta de forças
+                df12 = np.absolute(Tc)/2.0
+                # Forças f1 e f2 final f12' = f12 + deltf12
+                if (Tc >= 0.0):
+                    f12[0] = f12[0] + df12
+                    f12[1] = f12[1] - df12
+                else:
+                    f12[0] = f12[0] - df12
+                    f12[1] = f12[1] + df12
+                # Comando de rpm dos motores
+                w1_ = np.sqrt(f12[0]/(kf))
+                w2_ = np.sqrt(f12[1]/(kf))
+                #
+                # Limitando o comando do motor entre 0 - 15000 rpm
+                w1 = np.maximum(0., np.minimum(w1_, w_max))
+                w2 = np.maximum(0., np.minimum(w2_, w_max))
+                # Determinação do comando de entrada
+                w_[:,j] = np.array([w1, w2])
+                j = j+1
+            else:
+                phi_ = self.phibarra[-1]
+            # Simulação um passo a frente
+            #print("xk",x[:,k])
+            x[:,k+1] = self.rk4(tc[k], h, x[:,k], w_[:,j-1])
+            x[0,k+1] = np.maximum(0., np.minimum(x[0,k+1], w_max))
+            x[1,k+1] = np.maximum(0., np.minimum(x[1,k+1], w_max))
+            #print("xkp1", x[:,k+1])
+            # Processaento de variáveis intermediárias
+            # obtem a força aplicada por cada rotor
+            f = np.zeros([3, tam])
+            for k in range(tam):
+                w = x[0:2,k]
+                f[0:2,k] = np.array([kf*w[0]**2, kf*w[1]**2])
+                f[2,k] = f[0,k] + f[1,k] # Fc total em B
+            #print(np.max(x[:,0]), np.min(x[:,1]))
+            #print(np.max(f[:,0]), np.min(f[:,1]), np.min(f[:,2]))
+        
+        
+        if tam <=2:
+            phi_ = self.phibarra[-1]
+        for i in range(1,len(tc)):
+            self.phibarra += [phi_] 
+        self.w[0] = x[0,:][-1]
+        self.w[1] = x[1,:][-1]
+        self.r = np.concatenate((self.r, np.array([x[2,:][1::],x[3,:][1::]])),axis=1)
+        self.dr = np.concatenate((self.dr, np.array([x[4,:][1::],x[5,:][1::]])),axis=1)
+        self.phi = np.concatenate((self.phi, np.array(x[6,:][1::])*180/math.pi))
+        self.dphi = np.concatenate((self.dphi, np.array(x[7,:][1::])*180/math.pi))
+        for i in range(1,len(tc)):
+            tc[i] = self.t[-1]+tc[i]
+            self.lrbarra = np.concatenate((self.lrbarra,np.transpose(np.array([self.rbarra]))),axis=1)
+        self.t = np.concatenate((self.t,tc[1::]))
+
+        self.rebote()
 
     def rebote(self):
         if self.colisaoh == "right" and self.dr[:,-1][0] > 0:
-            self.dr[:,-1][0] = -self.dr[:,-1][0]*80/100
-            self.dr[:,-2][0] = -self.dr[:,-2][0]*80/100
+            self.dr[:,-1][0] = -self.dr[:,-1][0]
+            self.dr[:,-2][0] = -self.dr[:,-2][0]
             self.velocity[0] = -self.velocity[0]
             self.colisaoh = "none"
         elif self.colisaoh == "left" and self.dr[:,-1][0] < 0:
-            self.dr[:,-1][0] = -self.dr[:,-1][0]*80/100
-            self.dr[:,-2][0] = -self.dr[:,-2][0]*80/100
+            self.dr[:,-1][0] = -self.dr[:,-1][0]
+            self.dr[:,-2][0] = -self.dr[:,-2][0]
             self.velocity[0] = -self.velocity[0]
             self.colisaoh = "none"
         if self.colisaov == "up" and self.dr[:,-1][1] < 0:
-            self.dr[:,-1][1] = -self.dr[:,-1][1]*80/100
-            self.dr[:,-2][1] = -self.dr[:,-2][1]*80/100
+            self.dr[:,-1][1] = -self.dr[:,-1][1]
+            self.dr[:,-2][1] = -self.dr[:,-2][1]
             self.velocity[1] = -self.velocity[1]
             self.colisaov = "none"
         elif self.colisaov == "down" and self.dr[:,-1][1] > 0:
-            self.dr[:,-1][1] = -self.dr[:,-1][1]
+            self.dr[:,-1][1] = -self.dr[:,-1][1]*80/100
             self.dr[:,-2][1] = -self.dr[:,-2][1]*80/100
-            self.velocity[1] = 20
+            self.velocity[1] = -self.velocity[1]*80/100
             self.colisaov = "none"
         
 
 
 
 
+    def atualizar_dinamica_2(self, tempo):
+            #posicao e angulo na tela
+            self.atualizar_posicao_tela()
 
+            #FC e phibarra
+            self.Cp()
+            #Tc
+            self.Ca()
+            #wbarras
+            self.Cw()
+            
+
+            #dinamica robo
+            # Evoluindo a din. da planta
+            #condições iniciais
+            w0 = self.w
+            w0 = w0.tolist()
+            r0 = self.r[:,-1]
+            r0[1] = r0[1]
+            r0 = r0.tolist()
+            v0 = self.dr[:,-1]
+            v0[1] = v0[1]
+            v0 = v0.tolist()
+            phi0 = self.phi[-1]
+            dphi0 = self.dphi[-1]
+            x0 = [w0[0], w0[1], r0[0], r0[1], v0[0], v0[1], phi0, dphi0]   # condicao inicial
+
+            #TENTAR COM RK4
+            #calculo eq. dif.
+            sol = self.rk4(self.din_robo, x0, [self.t[-1], self.t[-1]+tempo], args = (self.wbarra, tempo))
+            #sol = odeint(self.din_robo, x0, [0.0, tempo], args=(self.wbarra,tempo,))
+            print("[w0[0], w0[1], r0[0], r0[1], v0[0], v0[1], phi0, dphi0] ")
+            print(x0)
+            print(sol[:,0][-1], sol[:,1][-1], sol[:,2][-1], sol[:,3][-1], sol[:,4][-1], sol[:,5][-1], sol[:,6][-1], sol[:,7][-1])
+            #solucao eq. dif.
+            self.w[0] = sol[:,0][-1]
+            self.w[1] = sol[:,1][-1]
+            r1 = np.transpose(np.array([[sol[:,2][-1],sol[:,3][-1]]]))
+            self.r = np.concatenate((self.r,r1), axis=1)
+            dr1 = np.transpose(np.array([[sol[:,4][-1],sol[:,5][-1]]]))
+            self.dr = np.concatenate((self.dr,dr1), axis=1)
+            self.phi += [sol[:,6][-1]]
+            self.dphi += [sol[:,7][-1]]
+            self.t += [self.t[-1] + tempo]
+
+            self.rebote()
 
 
 
